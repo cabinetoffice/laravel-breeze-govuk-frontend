f62bb2827efdfdccc62bfdc3360bfb6ddec208ad
Mizan Syed <mizansyed@gmail.com>
2022-09-27 16:51:37 +0100

    Update to Laravel 9 and GOV.UK Frontend 3.14

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -44,7 +44,7 @@ class InstallCommand extends Command
         $this->updateNodePackages(function ($packages) {
 
             return [
-                    'govuk-frontend' => '^3.10.0',
+                    'govuk-frontend' => '^3.14.0',
                     "cross-env" => "^7.0",
                     "laravel-mix" => "^5.0.9",
                     "resolve-url-loader" => "^2.3.1",
